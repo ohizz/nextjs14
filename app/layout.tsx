e408f9e9e5cb0dea81c2743789211090cfd1c4e3
ohizz <jtsamuel3084@gmail.com>
2024-02-12 13:17:35 +0100

    end next14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts'
+import SideNav from './ui/dashboard/sidenav';
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
     </html>
   );
 }
